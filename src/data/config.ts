1f4c67010390e4e047a11a4f6f5562c31c021f3c
Muunatic <ichigoez0@gmail.com>
2022-02-05 19:26:18 +0700

    2.1.0 Release

---
+++ src/data/config.ts
@@ -0,0 +1,3 @@
+export const osutoken = 'KEYAPI';
+export const prefix = 'PREFIX';
+export const DEFAULT_ERROR = '[0] - Error';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import fs = require('fs');
 import whatsapp = require('whatsapp-web.js');
+import malScraper = require('mal-scraper');
+import osu = require('node-osu');
+import { osutoken, prefix, DEFAULT_ERROR } from './data/config';
+const osuAPI = new osu.Api(osutoken, {
+    notFoundAsError: false,
+    completeScores: false,
+    parseNumeric: false
+})
 
-const SESSION_FILE_PATH = './whatsapplogin.json';
+process.on('unhandledRejection', error => {
+    console.error('Unhandled Promise Rejection:', error);
+});
+
+process.on('uncaughtException', error => {
+    console.error('uncaughtException:', error)
+});
+
+process.on('uncaughtExceptionMonitor', error => {
+    console.error('uncaughtExceptionMonitor:', error)
+});
+
+const SESSION_FILE_PATH = '../whatsapplogin.json';
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
@@ -10,7 +30,7 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
 const client = new whatsapp.Client({ puppeteer: { headless: false }, session: sessionCfg });
 
 client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr); // Required dom lib
+    console.log('QR RECEIVED', qr);
 });
 
 client.on('authenticated', (session) => {
@@ -27,16 +47,44 @@ client.on('ready', () => {
     console.log('Client Connected');
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
 
-    const command = msg.body 
+    const args = msg.body.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift()?.toLowerCase();
 
     const a = 'foo';
     const b = 'bar';
 
     if (command == a) {
         msg.reply(b);
-        console.info(msg.body)
+        console.info(msg.body);
+    }
+
+    if (command === 'osu') {
+        if (!args.join(' ')) return msg.reply(DEFAULT_ERROR);
+        const data = await osuAPI.getUser({
+            u: args[0], m: 3
+        });
+        if (!data) return msg.reply(DEFAULT_ERROR);
+        if (!data.pp.rank || !data.accuracy === null) return msg.reply(DEFAULT_ERROR);
+        const resp = 'Name: ' + data.name + '\n\n' + 'Rank: ' + data.pp.rank + '\n\n' + 'Level: ' + data.level + '\n\n' + 'Accuracy: ' + data.accuracy + '\n\n' + 'Joined: ' + data.raw_joinDate + '\n\n' + 'Performance Point: ' + data.pp.raw + '\n\n' + 'URL: ' + `https://osu.ppy.sh/users/${data.id}`;
+        const media = await whatsapp.MessageMedia.fromUrl(`https://a.ppy.sh/${data.id}`);
+        await msg.reply(media);
+        await msg.reply(resp);
+        console.info(msg.body);
+    }
+
+    if (command === 'anime') {
+        if (!args.join(' ')) return msg.reply(DEFAULT_ERROR);
+        const animevalue = args.join('');
+        const animescraper = await malScraper.getInfoFromName(animevalue);
+        if (!animescraper) return msg.reply(DEFAULT_ERROR);
+        const resp = 'Synopsis: ' + animescraper.synopsis;
+        const resp2 = 'Title: ' + animescraper.title + '\n\n' + 'Type: ' + animescraper.type + '\n\n' + 'Episode: ' + animescraper.episodes + '\n\n' + 'Duration: ' + animescraper.duration + '\n\n' + 'Genres: ' + animescraper.genres?.join(', ') + '\n\n' + 'Status: ' + animescraper.status + '\n\n' + 'Score: ' + animescraper.score;
+        const media = await whatsapp.MessageMedia.fromUrl(animescraper.picture || '');
+        await msg.reply(media);
+        await msg.reply(resp + '\n\n' + resp2);
+        console.info(msg.body);
     }
 
 });
